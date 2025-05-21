f8121089d5085e617826733bdf92dd4df53c89f0
jppessoal <jpmbcaxias@gmail.com>
2025-05-21 08:30:09 -0300

    Add files via upload

---
+++ Calculadora.java
@@ -0,0 +1,42 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package calculadora;
+
+/**
+ *
+ * @author ERICKFIGUEREDODESOUZ
+ */
+import java.util.Scanner;
+public class Calculadora {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("==Calculadora==");
+        Scanner ler = new Scanner(System.in);
+        System.out.println("Digite um número");
+        int n1 = ler.nextInt();
+        System.out.println("Digite outro número");
+        int n2 = ler.nextInt();
+        System.out.println("Digite a operação (+.-.*,/): ");
+        String op= ler.next();
+        if(op.equals ("+")){
+            int soma = n1 + n2;
+            System.out.println("A soma é "+soma);
+        }else if(op.equals("-")){
+            int sub = n1 - n2;
+            System.out.println("A Subtração é "+sub);
+        }else if(op.equals("*")){
+            int mult = n1 * n2;
+            System.out.println("A Multiplicação é "+mult);
+        }else if(op.equals("/")){
+            int div = n1 / n2;
+            System.out.println("A Divisão é "+div);
+        }
+    }
+    
+}
